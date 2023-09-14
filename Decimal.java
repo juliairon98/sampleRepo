c3cd74c8dd3550e1ebb9e75725213f7c9147c076
mylenearaoarao713 <mylenearaoarao.aws@gmail.com>
2023-09-14 17:24:04 +0800

    test

---
+++ Decimal.java
@@ -0,0 +1,16 @@
+package Variables;
+
+public class Decimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//don't use int for mathematical calculation, use double
+		double percentage = 73.5;
+		System.out.println(percentage + " of percentages are made.");
+		
+		double wallet = 20;
+		int people = 3;
+		System.out.println(wallet/people);
+	}
+
+}
